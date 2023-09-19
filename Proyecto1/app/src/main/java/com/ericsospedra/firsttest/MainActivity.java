267f4a2f69b02d5c77cdf424d306c739b3808234
Eric_212 <ericsospedrasalort212@gmail.com>
2023-09-19 13:47:28 +0200

    FirstTest

--- Proyecto1/app/src/main/java/com/ericsospedra/firsttest/MainActivity.java
+++ Proyecto1/app/src/main/java/com/ericsospedra/firsttest/MainActivity.java
@@ -3,12 +3,65 @@ package com.ericsospedra.firsttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main2);
+        Log.d(TAG,"OnCreate");
+
+        Button bSaluda = findViewById(R.id.Rsaluda);
+        bSaluda.setText("Saludos");
+
+        bSaluda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Hola",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"OnResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"OnRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"OnDestroy");
     }
 }
